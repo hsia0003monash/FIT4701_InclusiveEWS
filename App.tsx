e1a41cf95b59a159904951533ba2c2746f3a9457
kbis0003 <kbis0003@student.monash.edu>
2026-08-11 18:10:09 +1000

    change tab bar to allow navigation between pages move tab par from individual pages to app and implement logic to allow navigation. when adding a screen all that is needed is to add the tabkey and component pairing to the screens constant.

--- App.tsx
+++ App.tsx
@@ -8,13 +8,19 @@ import {
 } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { HomeScreen } from './src/screens/HomeScreen';
+import { MapScreen } from './src/screens/MapScreen';
+import { RTabBar, TabKey } from './src/components/RTabBar';
 
 SplashScreen.preventAutoHideAsync();
+const Screens: Record<TabKey, React.ComponentType> = {
+  Home: HomeScreen,
+  Map: MapScreen,
 
+}
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -23,6 +29,8 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold,
   });
+
+  const [activeTab, setActiveTab] = useState<TabKey>('Home')
 
   const onLayout = useCallback(async () => {
     if (fontsLoaded) {
@@ -34,10 +42,15 @@ export default function App() {
     return null;
   }
 
+  const ActiveScreen = Screens[activeTab]
+
   return (
     <SafeAreaProvider>
       <View style={{ flex: 1 }} onLayout={onLayout}>
-        <HomeScreen />
+        <View style={{ flex: 1 }}>
+          <ActiveScreen />
+        </View>
+        <RTabBar active={activeTab} onSelect={setActiveTab} />
         <StatusBar style="auto" />
       </View>
     </SafeAreaProvider>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -136,7 +136,6 @@ export function HomeScreen() {
           </RCard>
         </ScrollView>
       </SafeAreaView>
-      <RTabBar active="Home" />
     </View>
   );
 }
